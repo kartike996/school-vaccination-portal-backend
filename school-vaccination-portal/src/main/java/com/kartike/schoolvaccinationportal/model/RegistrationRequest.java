776179b48a5d6b0495333d9ab7bf85868c9d2be4
Kartike Agarwal <kartike.agarwal@concentrix.com>
2025-04-22 16:03:48 +0530

    Added models data

---
+++ school-vaccination-portal/src/main/java/com/kartike/schoolvaccinationportal/model/RegistrationRequest.java
@@ -0,0 +1,11 @@
+package com.kartike.schoolvaccinationportal.model;
+
+import lombok.Data;
+
+@Data
+public class RegistrationRequest {
+	
+	private String driveId;
+	
+    private String studentId;
+}
