81958ad72c84817b097c468478a0af45d4da47a4
Kartike Agarwal <kartike.agarwal@concentrix.com>
2025-04-22 15:38:32 +0530

    Initial commit

---
+++ school-vaccination-portal/src/main/java/com/kartike/schoolvaccinationportal/SchoolVaccinationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.kartike.schoolvaccinationportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolVaccinationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolVaccinationPortalApplication.class, args);
+	}
+
+}
---
+++ school-vaccination-portal/src/test/java/com/kartike/schoolvaccinationportal/SchoolVaccinationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kartike.schoolvaccinationportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolVaccinationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
